2d5fe0407c2b73b2c8d1510fc1dbe26fbc25e022
rjhigdon <rjocunts@protonmail.com>
2024-02-02 11:39:48 -0800

    added tables, basic flask, virtual environment

---
+++ key.sh
@@ -0,0 +1,1 @@
+apiKey = "c2b73060463b4faeba95b4febf2300e3"
--- model.py
+++ model.py
@@ -12,7 +12,14 @@ class User(db.Model):
     password = db.Column(db.String(25), nullable = False)
     email = db.Column(db.String(70), nullable = False)
     logged_in = db.Column(db.Boolean, nullable = False)
-    rating = db.Column(db.Integer, nullable = True)    
+    rating = db.Column(db.Integer, nullable = True)   
+    
+    def __init__(self, username, password, email, logged_in, rating):
+        self.username = username
+        self.password = password
+        self.email = email
+        self.logged_in = logged_in
+        self.rating = rating
 
 class Game(db.Model):
     
@@ -21,11 +28,20 @@ class Game(db.Model):
     id = db.Column(db.Integer, primary_key = True, autoincrement = True)
     title = db.Column(db.String(50), nullable = False)
     genre = db.Column(db.String(50), nullable = False)
-    vgConsole = db.Column(db.String(50), nullable = True)
-    releaseYr = db.Column(db.Integer, nullable = True)
-    playYr = db.Column(db.Integer, nullable = True)
+    vg_console = db.Column(db.String(50), nullable = True)
+    release_yr = db.Column(db.Integer, nullable = True)
+    play_yr = db.Column(db.Integer, nullable = True)
     description = db.Column(db.String(250), nullable = True) 
-    rating = db.Column(db.Integer, nullable = False)   
+    rating = db.Column(db.Integer, nullable = False) 
+    
+    def __init__(self, title, genre, vg_console, release_yr, play_yr, description, rating):
+        self.title = title
+        self.genre = genre
+        self. vg_console = vg_console
+        self.release_yr = release_yr
+        self.play_yr = play_yr
+        self.description = description
+        self.rating = rati
 
 class Rating(db.Model):
     
@@ -33,9 +49,15 @@ class Rating(db.Model):
     
     id = db.Column(db.Integer, primary_key = True, autoincrement = True)
     rating = db.Column(db.Integer, nullable = False)
+    
+    def __init__(self, rating):
+        self.rating = rating
 
 def connect_to_db(app):
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["POSTGRES_URI"]
+    try:
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["POSTGRES_URI"]
+    except KeyError:
+        print("provide POSTGRES_URI")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
@@ -45,4 +67,8 @@ if __name__ == "__main__":
     app = Flask(__name__)
     connect_to_db(app)
     print("Connected to db...")
+    
+with app.app_context():
+        db.create_all()
+        print("created Tables")
     
