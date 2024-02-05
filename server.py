e5e3f02e3439e953e41619a3930aa31bc9649776
rjhigdon <118936868+rjhigdon@users.noreply.github.com>
2024-02-05 15:43:28 -0800

    added some jinja and a couple badsic view functions

--- server.py
+++ server.py
@@ -1,7 +1,20 @@
 from flask import Flask
+from jinja2 import Environment, PackageLoader, select_autoescape
 
 app = Flask(__name__)
+app.secret_key = "SuperDuperSecretKey"
+
+env = Environment(
+    loader=PackageLoader("GameRanker"),
+    autoescape=select_autoescape()
+)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello World</p>"
+def homepage():
+    return render_template("homepage.html")
+
+@app.route("/login")
+def login():
+    pass
+
+@app.route()
