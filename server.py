4703bec6babe94a5e0c9b0457386cc233cd7245e
rjhigdon <rjocunts@protonmail.com>
2024-02-15 16:06:09 -0800

    relocated

--- server.py
+++ server.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template, flash
+from flask import Flask, render_template, flash, redirect
 from jinja2 import Environment, PackageLoader, StrictUndefined
 from forms import RegisterForm, LoginForm, AddGame
+from model import User, Game, db, connect_to_db
+from flask_login import LoginManager, login_required
 
 app = Flask(__name__)
 app.secret_key = "SuperDuperSecretKey"
 app.jinja_env.undefined = StrictUndefined
 
+login_manager = LoginManager
+login_manager.init_app(app)
 
 @app.route("/", methods=["GET", "POST"])
 def home():
@@ -15,14 +19,24 @@ def home():
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    user = User(username = form.username.data, password = form.password.data)
+    if form.validate_on_submit:
+        login(user)
+        flash(f"user: {user.username} logged in")
+        return redirect("/user")
     return render_template("login.html", form=form)
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
+    new_user = User(username = form.username.data, password = form.password.data)
+    db.session.add(new_user)
+    db.session.commit()
+    flash(f"new user {new_user.username} created successfully")
     return render_template("register.html", form=form)
 
 @app.route("/user") 
+@login_required
 def user():
     pass
 
